976a67096a3105a7cbc31b619e0a7cffb9acb0b3
Romain Laurent <laurent@mnhn.fr>
2019-03-20 15:13:54 +0100

    Removing useless code

--- include/set_params.h
+++ include/set_params.h
@@ -2,9 +2,6 @@
 #ifndef SET_PARAMS_H
 #define SET_PARAMS_H
 
-#define MIN_LAMBDA 0.001
-#define MAX_LAMBDA 4
-
 unsigned check_param_file_nb_field(FILE *f);
 void read_param_file(param *params, arg *args, unsigned idx_simul);
 void seed_RNGs_from_file(param *params, unsigned nb_rng);
@@ -14,7 +11,5 @@ void create_simul_log(arg *args, param *params, unsigned idx_simul);
 void reset_param(param *params, arg *args);
 void set_params_one_simul(param *params, arg *args, unsigned idx_simul);
 void reseed_rngs(param *params, unsigned nb_rng, char *mess);
-void draw_params(param *params, arg *args);
-
 
 #endif
